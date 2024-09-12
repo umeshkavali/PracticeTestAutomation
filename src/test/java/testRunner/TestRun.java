24974d079d5077a976a5164f44856ca8bb92c374
umeshkumar.kavali <umeshkumar.kavali@10.96.30.148>
2024-09-12 12:39:31 +0530

    Reports have been added to the existing Project

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		
-		features= {"C:\\Eclipse Projects\\Interview\\PracticeTestAutomation\\src\\test\\java\\features\\TC_001PositiveLogInTest.feature"},
+		features= {"C:\\Users\\umeshkumar.kavali\\git\\PracticeTestAutomation\\src\\test\\java\\features"},
 		glue="stepDefinitions",
 		plugin= {"pretty", "html:reports/myreport.html", 
 				"rerun:target/rerun.txt",
